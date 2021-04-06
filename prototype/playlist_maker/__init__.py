6be3608315bcfc80cd29f9dd99b5f712fa2b1a96
Jamie Dela Cruz <jamiedelacruz@dhcp-wifi-8021x-155-41-22-113.bu.edu>
2021-04-05 22:26:39 -0400

    tinkering with the code

--- prototype/playlist_maker/__init__.py
+++ prototype/playlist_maker/__init__.py
@@ -4,6 +4,25 @@ import requests
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+
+    ''' app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass '''
+
     # home route
     @app.route("/") 
     def hello(): # route handler function
@@ -35,4 +54,8 @@ def create_app():
         owner = data['owner']['display_name'],
         num_songs = data['tracks']['total'],
         playlist_image = data['images'][0]['url'])
+    
+    from . import auth
+    app.register_blueprint(auth.auth_bp)
+
     return app
