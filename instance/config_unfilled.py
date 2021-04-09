62b3a1a336420b3d502e588949479e21683eab7c
John Mikulskis <jkulskis@bu.edu>
2021-04-09 13:15:24 -0400

    added instance/config.py unfilled

---
+++ instance/config_unfilled.py
@@ -0,0 +1,9 @@
+#### NOTE: Copy this file into the instance folder, rename it config.py, and fill in the keys
+
+# spotify
+SPOTIFY_CLIENT_ID = ''
+SPOTIFY_CLIENT_SECRET = ''
+SPOTIFY_REDIRECT_URI = 'http://localhost:5000/callback/spotify' # change to domain after deployment
+SPOTIFY_SCOPE = 'user-library-read'
+# google
+GOOGLE_API_KEY = ''
