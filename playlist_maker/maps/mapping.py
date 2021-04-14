6e55a9e3feb6a8f8b1dc290f1db501c83ea8f66e
John Mikulskis <jkulskis@bu.edu>
2021-04-14 18:24:15 -0400

    changed mapping form to check type of request instead of sending form to new url

--- playlist_maker/maps/mapping.py
+++ playlist_maker/maps/mapping.py
@@ -4,26 +4,32 @@ from playlist_maker.spotify.spotify import SpotifyHandler
 
 map_blueprint = Blueprint('map_bp', __name__, template_folder='templates')
 
-@map_blueprint.route('/form')
+@map_blueprint.route('/mapping', methods=['GET'])
 def form():
-    spotify = SpotifyHandler()
-    if not spotify.valid_token():
-        return redirect('/')
-    return render_template('form.html', name=spotify.me()['display_name'])
+  spotify = SpotifyHandler()
+  if not spotify.valid_token():
+    return redirect('/')
+  if request.method == "GET":
+    return render_template(
+      'mapping.html', 
+      name=spotify.me()['display_name']
+    )
+  elif request.method == "POST":
+    return get_distance()
 
-@map_blueprint.route('/request', methods=['GET','POST'])
-def get_distance():
-    # set up gmaps client
-    google_api_key = current_app.config['GOOGLE_API_KEY']
-    gmaps = googlemaps.Client(key=google_api_key)
-    # grab the form values
-    origin = request.form['origin']
-    destination = request.form['destination']
-    mode = request.form['mode']
-    # get the response from gmaps
-    data = gmaps.distance_matrix(origin, destination, mode=mode, region='US')
-    duration = data["rows"][0]["elements"][0]["duration"]["value"]
+@map_blueprint.route('/mapping', methods=['POST'])
+def display_distance():
+  # set up gmaps client
+  google_api_key = current_app.config['GOOGLE_API_KEY']
+  gmaps = googlemaps.Client(key=google_api_key)
+  # grab the form values
+  origin = request.form['origin']
+  destination = request.form['destination']
+  mode = request.form['mode']
+  # get the response from gmaps
+  data = gmaps.distance_matrix(origin, destination, mode=mode, region='US')
+  duration = data["rows"][0]["elements"][0]["duration"]["value"]
 
-    return render_template('display_distance.html', data=data, duration=duration)
+  return render_template('display_distance.html', data=data, duration=duration)
 
 
