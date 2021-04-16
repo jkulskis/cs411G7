04647414e4f918fd61f69d65560c73b63c3cf661
John Mikulskis <jkulskis@bu.edu>
2021-04-15 21:58:58 -0400

    added spotify caches dir to config template

--- instance/config_unfilled.py
+++ instance/config_unfilled.py
@@ -5,5 +5,6 @@ SPOTIFY_CLIENT_ID = ''
 SPOTIFY_CLIENT_SECRET = ''
 SPOTIFY_REDIRECT_URI = 'http://localhost:5000/callback/spotify' # change to domain after deployment
 SPOTIFY_SCOPE = 'user-library-read'
+SPOTIFY_CACHES_DIR = './.spotify_caches'
 # google
 GOOGLE_API_KEY = ''
