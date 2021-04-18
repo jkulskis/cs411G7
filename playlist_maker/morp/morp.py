fd038d6bbc980681c3ad7e7a3d60a4650062bcec
aahwangxyz <aaron.hwang@xyzrobotics.ai>
2021-04-18 15:53:23 -0400

    Adding weather API

--- playlist_maker/morp/morp.py
+++ playlist_maker/morp/morp.py
@@ -1,6 +1,7 @@
 import googlemaps
 from flask import Blueprint, request, redirect, render_template, current_app, session, url_for
 from playlist_maker.spotify.spotify import SpotifyHandler
+from pyowm.owm import OWM
 
 morp_blueprint = Blueprint('morp_bp', __name__, template_folder='templates')
 
@@ -15,6 +16,24 @@ def form():
       name=session['display_name'],
       user_choices=session['user_choices']
     )
+  
+  # weather api
+  owm = OWM('46aea01cbd4afd5057ea0fbb49d6e0a3')
+  mgr = owm.weather_manager()
+
+  # get current location from cache
+  google_api_key = current_app.config['GOOGLE_API_KEY']
+  gmaps = googlemaps.Client(key=google_api_key)
+  # grab the form values
+  origin = session['user_choices']['origin']
+  origin_coded = gmaps.geocode(origin)
+  origin_lat = origin_coded[0]["geometry"]["location"]["lat"]
+  origin_long = origin_coded[0]["geometry"]["location"]["lng"]
+
+  weather = mgr.weather_at_coords(origin_lat, origin_long).weather
+  temp = weather.temperature('fahrenheit')["temp"]
+  weather_details = weather.detailed_status
+
   # POST
   if 'music' in request.form:
     session['user_choices']['morp'] = 'music'
@@ -38,4 +57,8 @@ def form():
   <i>Speed</i>: {session['user_choices']['speed']}
   <br>
   <i>Music or Podcast</i>: {session['user_choices']['morp']}
+  <br>
+  <i>Temperature</i>: {temp}
+  <br>
+  <i>Weather</i>: {weather_details}
   """
