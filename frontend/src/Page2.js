2bd34dbe20dd32203884589f11fce1cc2dcb6464
John Mikulskis <jkulskis@bu.edu>
2021-04-28 20:54:17 -0400

    fixed display_name localStorage example

--- frontend/src/Page2.js
+++ frontend/src/Page2.js
@@ -1,38 +1,46 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./App.css";
+import React from 'react';
+import {Link } from "react-router-dom";
+import './App.css';
 
 function Page2() {
-  return (
+    return (
     <form action="/request" method="POST">
-      <div className="container">
-        <center>
-          <h1>Welcome {localStorage.display_name}!</h1>
-        </center>
+        <div className="container">
+        <center><h1>Welcome name !</h1></center>
         <br />
-        <center>
-          <h2>Input the following information</h2>
-        </center>
+        <center><h2>How are you getting there?</h2></center>
         <br />
         <hr></hr>
-        <label>Mode of Transportation:</label>
-        <select>
-          <option value="Mode of transportation">Mode of transportation</option>
-          <option value="Walking">Walking</option>
-          <option value="Biking">Biking</option>
-          <option value="Driving">Driving</option>
-        </select>
-        <br />
-        <br />
+        <label>
+        Mode of Transportation: 
+        </label>
+        <br></br>
+        <br></br>
 
+        <input type="buttonforms" class="buttonforms" value="Walking"></input>
+        <input type="buttonforms" class="buttonforms" value="Biking"></input>
+        <input type="buttonforms" class="buttonforms" value="Driving"></input>
+
+{/*         <select>
+            <option value=" "> </option>
+            <option value="Walking" class="buttonforms">Walking</option>
+            <option value="Biking" class="buttonforms">Biking</option>
+            <option value="Driving" class="buttonforms">Driving</option>
+        </select>
+        <br /><br /> */}
+        <br></br>
+        <br></br>        
+        <br></br>
+        <br></br>
+        
         <center>
-          <Link to="/page3">
-            <button className="button">Next</button>
-          </Link>
+            <Link to="/page1"><button className="buttonback"> Go Back </button></Link>
+            <Link to="/page3"><button className="button"> Next Page </button></Link>
         </center>
-      </div>
+
+        </div>
     </form>
-  );
+    );
 }
 
 export default Page2;
